6468f4aa2be09ca2fcc9247b2fcb9d105f4741de
Mark Cameron <mark.cameron@devfactory.ch>
2021-05-24 15:28:48 +0200

    Add some loading animations to improve UX

--- resources/views/livewire/predictions-list.blade.php
+++ resources/views/livewire/predictions-list.blade.php
@@ -1,13 +1,22 @@
 <div class="m-4">
 
-    @if ($showPredictionForm)
-        <livewire:prediction :match="$match" />
-    @else
+    <div wire:loading.flex class="text-white h-96 flex items-center justify-center">
+        <svg class="animate-spin -ml-1 mr-3 h-16 w-16 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-30" cx="12" cy="12" r="10" stroke="white" stroke-width="4"></circle>
+            <path class="opacity-75" fill="white" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+    </div>
 
-        @foreach ($matches as $match)
-            <livewire:prediction-row :match="$match"/>
-        @endforeach
+    <div wire:loading.remove>
+        @if ($showPredictionForm)
+            <livewire:prediction :match="$match" />
+        @else
 
-    @endif
+            @foreach ($matches as $match)
+                <livewire:prediction-row :match="$match"/>
+            @endforeach
+
+        @endif
+    </div>
 
 </div>
