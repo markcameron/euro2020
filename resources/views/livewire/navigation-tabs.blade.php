d36877cabfd63f4ec84341e03a105834cbf451a1
Mark Cameron <mark.cameron@devfactory.ch>
2021-05-31 22:09:01 +0200

    Add loading screen between tab switch

--- resources/views/livewire/navigation-tabs.blade.php
+++ resources/views/livewire/navigation-tabs.blade.php
@@ -6,7 +6,14 @@
 
     <main class="overflow-y-auto mb-auto flex-grow bg-gradient-to-b from-euro-darkest to-euro-light" id="scrollable">
 
-        <main>
+        <div wire:loading.flex class="text-white h-96 flex items-center justify-center">
+            <svg class="animate-spin -ml-1 mr-3 h-16 w-16 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-30" cx="12" cy="12" r="10" stroke="white" stroke-width="4"></circle>
+                <path class="opacity-75" fill="white" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
+
+        <main wire:loading.remove>
             @if ($selectedTab === 1)
                 <livewire:match-list />
             @elseif ($selectedTab === 2)
